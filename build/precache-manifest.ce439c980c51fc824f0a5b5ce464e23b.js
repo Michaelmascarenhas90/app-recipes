fb5794049e4c09bb3ce6ef45edcb26e12f0933ca
Michael Mascarenhas <michaelmascarenhastrybe@gmail.com>
2021-09-22 12:46:06 -0300

    arquivos de build

---
+++ build/precache-manifest.ce439c980c51fc824f0a5b5ce464e23b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1e53ba9d33fee4bbdb87aa2620a0551",
+    "url": "./index.html"
+  },
+  {
+    "revision": "980c746ebc84a4a21146",
+    "url": "./static/css/2.95258f03.chunk.css"
+  },
+  {
+    "revision": "27415eb86ab9cedae2d9",
+    "url": "./static/css/main.7d3e33c2.chunk.css"
+  },
+  {
+    "revision": "980c746ebc84a4a21146",
+    "url": "./static/js/2.1140838e.chunk.js"
+  },
+  {
+    "revision": "9353b4df7938eafd5f8e3a0cf5a68c12",
+    "url": "./static/js/2.1140838e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27415eb86ab9cedae2d9",
+    "url": "./static/js/main.013b92b0.chunk.js"
+  },
+  {
+    "revision": "c8d062e20b9c54fa3852",
+    "url": "./static/js/runtime-main.5e8093d5.js"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "./static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "./static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "./static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "./static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "./static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "2fd40f14774dc7e188c6340be0b0925a",
+    "url": "./static/media/recipes.2fd40f14.png"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "./static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "./static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "./static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
